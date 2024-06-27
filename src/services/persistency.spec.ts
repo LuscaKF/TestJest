4d058ee6ed8b8eed509a6bc3bc504babdbd7f457
LuscaKF <lucasfernandesf18@gmail.com>
2024-06-26 22:34:29 -0300

    Introdução ao Jest

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('Testando um number', () => {
+  it('Should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('Testando uma string', () => {
+  test('Should return Lucas', () => {
+    const nome = 'Lucas';
+    expect(nome).toBe('Lucas');
+  });
+});
