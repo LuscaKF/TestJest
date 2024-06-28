879cdaba3d2fb2d9010d2bed928c53067993c31a
LuscaKF <lucasfernandesf18@gmail.com>
2024-06-27 22:35:23 -0300

    Primeiro teste com spyOn

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -1,13 +1,25 @@
-describe('Testando um number', () => {
-  it('Should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('Testando uma string', () => {
-  test('Should return Lucas', () => {
-    const nome = 'Lucas';
-    expect(nome).toBe('Lucas');
+describe('Persistency', () => {
+  afterEach(() => jest.clearAllMocks());
+
+  it('Should return undefined', () => {
+    // System under test
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined();
+  });
+
+  it('Should call console.log once', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should call console.log once with "Pedido salvo com sucesso..."', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    expect(consoleSpy).toHaveBeenCalledWith('Pedido salvo com sucesso...');
   });
 });
