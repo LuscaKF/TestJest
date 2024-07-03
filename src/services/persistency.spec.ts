865cbb54e749308b9cf81ff6d700ce0aa23938e0
LuscaKF <lucasfernandesf18@gmail.com>
2024-07-03 19:14:57 -0300

    Finalizando os devidos testes com Jest

--- src/services/persistency.spec.ts
+++ src/services/persistency.spec.ts
@@ -3,20 +3,20 @@ import { Persistency } from './persistency';
 describe('Persistency', () => {
   afterEach(() => jest.clearAllMocks());
 
-  it('Should return undefined', () => {
+  it('should return undefined', () => {
     // System under test
     const sut = new Persistency();
     expect(sut.saveOrder()).toBeUndefined();
   });
 
-  it('Should call console.log once', () => {
+  it('should call console.log once', () => {
     const sut = new Persistency();
     const consoleSpy = jest.spyOn(console, 'log');
     sut.saveOrder();
     expect(consoleSpy).toHaveBeenCalledTimes(1);
   });
 
-  it('Should call console.log once with "Pedido salvo com sucesso..."', () => {
+  it('should call console.log with "Pedido salvo com sucesso..."', () => {
     const sut = new Persistency();
     const consoleSpy = jest.spyOn(console, 'log');
     sut.saveOrder();
